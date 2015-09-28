195509d95b7e60b4c5282be6e959976a9517fa1f
Thomas Blitz <tbl@infomedia.dk>
2015-09-28 02:16:57 +0200

    Spelling error in assembly info

--- TLogik.Krypt/Properties/AssemblyInfo.cs
+++ TLogik.Krypt/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TLogik.Krypt")]
-[assembly: AssemblyDescription("Validate and encrrypt passwords based on hashing and salting passwords. Use for basic authentication.")]
+[assembly: AssemblyDescription("Validate and encrypt passwords based on hashing and salting passwords. Use for basic authentication.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thomas Blitz")]
 [assembly: AssemblyProduct("TLogik.Krypt")]
